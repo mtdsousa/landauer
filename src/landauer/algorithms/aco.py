c8d77b91cb74e947fb4ba4ed420ccb26dc00868f
Marco Sousa <mtdsousa@gmail.com>
2023-07-14 09:59:47 -0300

    Update ACO algorithm

--- src/landauer/algorithms/aco.py
+++ src/landauer/algorithms/aco.py
@@ -23,112 +23,73 @@ SOFTWARE.
 '''
 
 import argparse
+import csv
+import functools
 import json
 import logging
-import sys
-
 import operator
 import networkx as nx
-import pandas as pd
 import random
+import sys
+import timeit
 
-import landauer.traversal as traversal
-import landauer.evaluate as evaluate
+import landauer.traversal
+import landauer.evaluate
+import landauer.placement
 
 def _update_pheromone(graph, default, evaporation, pmin, pmax, fitness, path):
+    '''
+    Updates the pheromone values for each edge in 'path'.
+    The new pheromone value is proportional (according to 'evaporation') to the last pheromone and fitness value.
+    For edges that don't have a pheromone value yet, we'll use 'default' as the last pheromone.
+    The new pheromone value is upper and lower bounded by 'pmax' and 'pmin' respectively.
+    '''
     for u, v in path:
         last_pheromone = graph.edges[u, v].get('weight', default)
         new_pheromone = (1 - evaporation) * last_pheromone + evaporation * fitness
         graph.edges[u, v]['weight'] = max(min(new_pheromone, pmax), pmin)
 
-def ant_colony(aig, entropy, fitness, ants, iterations, default, evaporation, pmin, pmax):
-    traversal_ = traversal.Traversal(aig)
-    
-    overall_results = list()
-    for iteration in range(iterations):
-        iteration_results = list()
-        for ant in range(ants):
-            path = list()
-            traversal.restart(traversal_)
-            while not traversal.leaf(traversal_.graph, traversal_.node):
-                successors = list(traversal_.graph.successors(traversal_.node))
-                pheromones = [traversal_.graph.edges[traversal_.node, successor].get('weight', default) for successor in successors]
-                successor = random.choices(successors, weights=pheromones)[0]
-                path.append((traversal_.node, successor))
-                traversal_.next(successor)
+def _iteration(ants, default, traversal, entropy, fitness):
+    '''
+    Run a single iteration. Ants will traverse the graph using pheromone values as probability.
+    This function will return the best path found.
+    '''
+    results = list()
+    for ant in range(ants):
+        path = list()
+        landauer.traversal.restart(traversal)
+        while not landauer.traversal.leaf(traversal.graph, traversal.node):
+            successors = list(traversal.graph.successors(traversal.node))
+            pheromones = [traversal.graph.edges[traversal.node, successor].get('weight', default) for successor in successors]
+            successor = random.choices(successors, weights=pheromones)[0]
+            path.append((traversal.node, successor))
+            traversal.next(successor)
 
-            delay = len(nx.dag_longest_path(traversal_.forwarding))
-            loss = evaluate.evaluate(traversal_.forwarding, entropy)['total']
-            iteration_results.append((delay, loss, fitness(delay, loss), path))
+        delay = len(nx.dag_longest_path(traversal.forwarding))
+        loss = landauer.evaluate.evaluate(traversal.forwarding, entropy)['total']
+        results.append((delay, loss, fitness(delay, loss), path))
+    return max(results, key = operator.itemgetter(2))
 
-        iteration_result = max(iteration_results, key = operator.itemgetter(2))
-        _update_pheromone(traversal_.graph, default, evaporation, pmin, pmax, iteration_result[2], iteration_result[3]),
-        logging.info(f"Iteration {iteration}: \n\tDelay: {iteration_result[0]}\n\tLoss: {iteration_result[1]}\n\tFitness: {iteration_result[2]}")
-        overall_results.append(iteration_result)
+def _run(traversal, ants, entropy, fitness, iterations, default, evaporation, pmin, pmax, dump):
+    '''
+    Run multiple iterations. For each iteration, it updates pheromone values given the best path found.
+    It returns a list with the best path found on each iteration as a 4-tuple element: delay, loss, fitness value and the path itself.
+    '''
+    for iteration in range(1, iterations + 1):
+        start = timeit.default_timer()
+        result = _iteration(ants, default, traversal, entropy, fitness)
+        delay, loss, fitness_value, path = result
+        _update_pheromone(traversal.graph, default, evaporation, pmin, pmax, fitness_value, path)
+        time = timeit.default_timer() - start
+        logging.info(f"iteration {iteration}:\n\ttime: {time}s\n\tdelay: {delay}\n\tloss: {loss}\n\tfitness: {fitness_value}")
+        dump(iteration, time, delay, loss, fitness_value, landauer.placement.placement(traversal.forwarding))
 
-    overall_result = max(overall_results, key = operator.itemgetter(2))
-    traversal.follow(traversal_, overall_result[3])
-    return traversal_.forwarding, traversal_.graph, overall_result, overall_results
+def ant_colony(aig, entropy, fitness, ants, iterations, default, evaporation, pmin, pmax, dump):
+    traversal = landauer.traversal.Traversal(aig)
+    return _run(traversal, ants, entropy, fitness, iterations, default, evaporation, pmin, pmax, dump)
 
-def _dump(id, header, results, output):
-    data = map(lambda result: (result[0], result[1], result[2], json.dumps([v for u, v in result[3]])), results)
-
-
-def benchmark(aig, entropy, settings):
-
-    import landauer.evaluate as evaluate
-    import pandas as pd
-
-    from timeit import default_timer as timer
-
-    data = list()
-    runs = settings.get('runs', 10)
-    ants = settings.get('ants', 1)
-    iterations = settings.get('iterations', 1)
-    default = settings.get('default', 0.5)
-    evaporation = settings.get('evaporation', 0.3)
-    pmin = settings.get('pmin', 0.001)
-    pmax = settings.get('pmax', 0.999)
-    graph = settings.get('graph', None)
-    report = settings.get('report', None)
-
-    for i in range(runs):
-        start = timer()
-        forwarding, graph, result, report = ant_colony(aig, entropy, \
-            fitness = lambda delay, loss: 1.0/(delay * settings.get('delay_weight', 0) + loss * settings.get('loss_weight', 1)), \
-            ants = ants, \
-            iterations = iterations, \
-            default = default, \
-            evaporation = evaporation, \
-            pmin = pmin, \
-            pmax = pmax)
-
-        time = timer() - start
-        delay, loss, fitness, _ = result
-        data.append((delay, loss, fitness, time))
-
-    df = pd.DataFrame(data, columns=['delay', 'loss', 'fitness', 'time'])
-    
-    avg = df.mean()
-    std = df.std()
-
-    return {
-        "runs": runs,
-        "time" : df['time'].sum(),
-        "ants" : ants,
-        "iterations" : iterations,
-        "default" : default,
-        "evaporation" : evaporation,
-        "pmin" : pmin,
-        "pmax" : pmax,
-        "avg_delay" : avg['delay'],
-        "std_delay" : std['delay'],
-        "avg_loss" : avg['loss'],
-        "std_loss" : std['loss'],
-        "avg_fitness" : avg['fitness'],
-        "std_fitness" : std['fitness'],
-        "min_fitness" : df['fitness'].min()
-    }
+def dump(writer, run, iteration, time, delay, loss, fitness, placement):
+    writer.writerow([run, iteration, time, delay, loss, fitness, landauer.placement.serialize(placement)])
 
 def main():
     argparser = argparse.ArgumentParser()
@@ -138,8 +99,9 @@ def main():
     group.add_argument('--file', help='and-inverter graph file', type=argparse.FileType('r'))
     group.add_argument('--stdin', help='read input data (and-inverter graph file) from stdin', action='store_true')
 
-    argparser.add_argument('--graph', help='output graph', type=argparse.FileType('w'))
-    argparser.add_argument('--report', help='output report', type=argparse.FileType('w'))
+    argparser.add_argument('--debug', help='print debug information', action='store_true')
+    argparser.add_argument('--output', help='dump results', type=argparse.FileType('w'))
+    argparser.add_argument('--runs', help='number of runs', type=int, default=1)
 
     argparser.add_argument('--ants', help='number of ants', type=int, default=1)
     argparser.add_argument('--iterations', help='number of iterations', type=int, default=1)
@@ -147,10 +109,8 @@ def main():
     argparser.add_argument('--evaporation', help='evaporation', type=float, default=0.3)
     argparser.add_argument('--min', help='min pheromone', type=float, default=0.001)
     argparser.add_argument('--max', help='max pheromone', type=float, default=0.999)
-    argparser.add_argument('--delay_weight', help='delay weight', type=float, default=0.0)
-    argparser.add_argument('--loss_weight', help='loss weight', type=float, default=1.0)
+    argparser.add_argument('--weight', help='weight applied to loss value', type=float, default=1.0)
 
-    argparser.add_argument('--debug', help='print debug information', action='store_true')
     args = argparser.parse_args()
     
     if args.debug:
@@ -163,19 +123,18 @@ def main():
     import landauer.entropy as entropy
     entropy_ = entropy.deserialize(args.entropy.read())
 
-    forwarding, graph, result, report = ant_colony(aig, entropy_, \
-        fitness = lambda delay, loss: 1.0/(delay * args.delay_weight + loss * args.loss_weight), \
-        ants = args.ants, \
-        iterations = args.iterations, \
-        default = args.default, \
-        evaporation = args.evaporation, \
-        pmin = args.min, \
-        pmax = args.max)
-
-    if args.graph is not None:
-        args.graph.write(json.dumps(nx.readwrite.json_graph.adjacency_data(graph)))
-
-    print(parse.serialize(forwarding))
+    writer = csv.writer(args.output if args.output != None else sys.stdout)
+    writer.writerow(['run', 'iteration', 'time', 'delay', 'loss', 'fitness', 'placement'])
+    for run in range(1, args.runs + 1):
+        results = ant_colony(aig, entropy_,
+            fitness = lambda delay, loss: 1.0/(delay * (1 - args.weight) + loss * args.weight),
+            ants = args.ants,
+            iterations = args.iterations,
+            default = args.default,
+            evaporation = args.evaporation,
+            pmin = args.min,
+            pmax = args.max,
+            dump = functools.partial(dump, writer, run))
 
 if __name__ == '__main__':
     main()
